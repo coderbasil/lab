0247549ebb3da16ac2c5846569974a133d81a6d4
Basil <bsuli1424@gmail.com>
2023-09-25 17:11:20 +0300

    added selection sort

--- Sorter.java
+++ Sorter.java
@@ -1,3 +1,32 @@
 public class Sorter {
+    
+        public static void selectionSort(int[] arr) {
+            int n = arr.length;
+    
+            // One by one move boundary of unsorted subarray
+            for (int i = 0; i < n - 1; i++) {
+                // Find the minimum element in the unsorted array
+                int minIdx = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[minIdx]) {
+                        minIdx = j;
+                    }
+                }
+    
+                // Swap the found minimum element with the first element of the subarray
+                int temp = arr[minIdx];
+                arr[minIdx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    
+        public static void main(String[] args) {
+            int[] arr = {64, 25, 12, 22, 11};
+            selectionSort(arr);
+            System.out.println("Sorted array:");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
 
 }
